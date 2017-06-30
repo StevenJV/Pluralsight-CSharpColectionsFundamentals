605b5e72d5523bd34f18c72c5776cc896a6accfd
StevenJV <StevenVore@tnwinc.com>
2017-06-30 12:09:04 -0400

    dictionary keys are case-sensitive by deafult

---
+++ CollectionsFun/CollectionsFun/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsFun
+{
+  class Program
+  {
+    static void Main(string[] args){
+      var primeMinisters = new Dictionary<string, string> {
+        {"JC", "James Callagha"},
+        {"MT", "Margaret Thacher"},
+        {"TB", "Tony Blair"}
+      };
+      primeMinisters["jc"] = "Jim Callaghan";  
+
+      foreach (var pm in primeMinisters) Console.WriteLine(pm);
+
+
+
+    }
+  }
+}
