7731c94deefd096dbdc7e3680e7212651f282609
StevenJV <StevenVore@tnwinc.com>
2017-06-30 12:10:57 -0400

    change behaviour by specifying a comparer

--- CollectionsFun/CollectionsFun/Program.cs
+++ CollectionsFun/CollectionsFun/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollectionsFun
 {
   class Program
   {
     static void Main(string[] args){
-      var primeMinisters = new Dictionary<string, string> {
+      var primeMinisters = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
         {"JC", "James Callagha"},
         {"MT", "Margaret Thacher"},
         {"TB", "Tony Blair"}
